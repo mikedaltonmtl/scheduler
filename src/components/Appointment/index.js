dce10aff72941f0d065520d72be231ffbfb3d4b9
miledaltonmtl <michael.j.dalton@gmail.com>
2023-01-28 10:37:12 -0500

    add Empty and Show (booked) Appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import "components/Appointment/styles.scss";
-// import InterviewerListItem from "components/InterviewerListItem";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 
 export default function Appointment(props) {
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ?
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        /> :
+        <Empty />}
+    </article>
   );
 };
 
+/*
+return (props.yourName ? <h1>Hello, {props.yourName}.</h1> : <h1>Sorry, you don't seem to have a name.</h1>);
 
+If props.interview (an interview object) is truthy the <Appointment> 
+will render the <Show> component, else it should render the <Empty> component.
+
+*/
